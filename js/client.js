7fe03565c9771780ed2411fc34736947f9843b5e
Floriancrt <florian.courthieu@gmail.com>
2019-09-29 18:36:48 +0200

    UPD: Add Users & New Messages

--- js/client.js
+++ js/client.js
@@ -1,6 +1,8 @@
 (function($){
 
     var socket = io.connect('http://localhost:1337');
+    var msg = $('#msgtpl').html();
+    $('#msgtpl').remove();
 
     $('#loginform').submit(function(event){
         event.preventDefault();
@@ -14,8 +16,32 @@
         $('#login').fadeOut();
     });
 
+    /**
+     * Envois de message
+     */
+    $('#form').submit(function(event){
+        event.preventDefault();
+        socket.emit('newmsg', {message: $('#message').val()});
+        $('#message').val('');
+        $('#message').focus();
+    });
+
+    socket.on('newmsg', function(message) {
+        $('#messages').append('<div class="message">' + Mustache.render(msg, message) + '</div>');
+    });
+
+
+    /**
+     * Gestion des connectés
+     * 
+     */
+
     socket.on('newusr', function(user){
-        $('#users').append('<img src="' + user.avatar +'">');
+        $('#users').append('<img src="' + user.avatar +'" id="' + user.id + '">');
+    })
+
+    socket.on('disusr', function(user){
+        $('#' + user.id).remove();
     })
 
 })(jQuery);
--- server.js
+++ server.js
@@ -10,18 +10,59 @@ httpServer = http.createServer((req, res) => {
 httpServer.listen(1337);
 
 var io = require('socket.io').listen(httpServer);
+var users = {};
+var messages = [];
+
 
 io.sockets.on('connection', function(socket) {
 
+    var me = false;
     console.log('Nouveau utilisateur');
+
+    for(var k in users)
+    {
+        socket.emit('newusr',users[k]);
+    }
+
+    for (var k in messages) {
+        socket.emit('newmsg', messages[k]);
+    }
+
+    /**
+     * On a reçu un message 
+     */
+    socket.on('newmsg',function(message){
+        message.user = me;
+        date = new Date();
+        message.h = date.getHours();
+        message.m = date.getMinutes();
+        io.sockets.emit('newmsg',message);
+    })
+
+    /**
+     * Je me connecte
+     */
 
     socket.on('login',function(user){
         me = user;
         me.id = user.mail.replace('@','-').replace('.','-');
         me.avatar = 'https://gravatar.com/avatar/' + md5(user.mail) + '?s=50';
         socket.emit('logged');
+        users[me.id] = me;
         io.sockets.emit('newusr',me);
     })
+
+    /**
+     * Je quitte le tchat
+     */
+    socket.on('disconnect',function(){
+        if(!me)
+        {
+            return false;
+        }
+        io.sockets.emit('disusr',me);
+        delete users[me.id];
+    });
 
 
 });
